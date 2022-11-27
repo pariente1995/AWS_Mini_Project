1ccef49531a19b097787c074be252dccacfeedd1
pariente1995 <pariente@naver.com>
2022-11-28 01:23:18 +0900

    프로젝트 초기 테스트_20221128_박세혁

---
+++ Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.sagaji.shoppingmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//	
+//	/**
+//	 * Simply selects the home view to render by returning its name.
+//	 */
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		return "home";
+//	}
+	
+	// 메인페이지로 이동(mainPage.jsp)
+	@GetMapping
+	public String main() {
+		return "mainPage";
+	}
+}
