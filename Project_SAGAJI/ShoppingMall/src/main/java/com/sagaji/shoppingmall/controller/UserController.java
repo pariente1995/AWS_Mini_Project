2a2a7b179a868aa7a4a4cdffbb7b2407acb39214
JAYEONG0423 <hello.git.jayeong423@gmail.com>
2022-11-29 19:31:49 +0900

    User클래스, Usermapper 및sql-map-config 수정

--- Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/controller/UserController.java
+++ Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.sagaji.shoppingmall.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sagaji.shoppingmall.service.user.UserService;
 import com.sagaji.shoppingmall.vo.UserVO;
 
 @Controller
-@RequestMapping
+@RequestMapping("/user")
 public class UserController {
 	
 	@Autowired
@@ -27,5 +32,41 @@ public class UserController {
 			model.addAttribute("joinMsg", "회원가입에 성공했습니다. 로그인해주세요.");
 			return "user/login";
 		}
-
+		//로그인 처리
+		
+		@GetMapping("/login.do")
+		public String login(UserVO userVO) {
+			return "/user/login";
+		}
+		@PostMapping("/login.do")
+		@ResponseBody
+		//HttpSession: 현재 WAS에 접속한 유저의 세션정보를 담고있는 객체
+		//			   세션에서 사용할 데이터를 담아줄 수 있다.
+		public String login(UserVO userVO, HttpSession session) {
+			//1. 아이디 체크
+			int idCheck = userService.idCheck(userVO.getUserId());
+			
+			if(idCheck < 1) {
+				return "idFail";
+			} else {
+				UserVO loginUser = userService.login(userVO);
+				
+				//2. 비밀번호 체크
+				if(loginUser == null) {
+					return "pwFail";
+				}
+				
+				//3. 로그인 성공
+				//세션에 로그인한 유저의 정보를 담아서 관리
+				session.setAttribute("loginUser", loginUser);
+				return "loginSuccess";
+			}
+		}
+		
+		@GetMapping("/logout.do")
+		public String logout(HttpSession session) {
+			session.invalidate();
+			
+			return "redirect:/main/main.jsp";
+		}
 }
--- Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/service/user/impl/UserServiceImpl.java
+++ Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/service/user/impl/UserServiceImpl.java
@@ -1,5 +1,30 @@
 package com.sagaji.shoppingmall.service.user.impl;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.sagaji.shoppingmall.service.user.UserService;
+import com.sagaji.shoppingmall.vo.UserVO;
+
+@Service
+public class UserServiceImpl implements UserService{
+	@Autowired
+	private UserDAO userDAO;
+	
+	@Override
+	public int join(UserVO userVO) {
+		return userDAO.join(userVO);
+	}
+
+	@Override
+	public int idCheck(String userId) {
+		return userDAO.idCheck(userId);
+	}
+
+	@Override
+	public UserVO login(UserVO userVO) {
+		return userDAO.login(userVO);
+	}
+
+	
 }
