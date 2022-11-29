dee5a1471efbc4859985f35b8a4ca223d513278a
seonminz <hello.git.sma708@naver.com>
2022-11-29 17:47:58 +0900

    CartController~DAO_asm

--- Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/controller/CartController.java
+++ Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/controller/CartController.java
@@ -1,5 +1,38 @@
 package com.sagaji.shoppingmall.controller;
 
-public class CartController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sagaji.shoppingmall.service.cart.CartService;
+import com.sagaji.shoppingmall.vo.CartVO;
+
+public class CartController {
+	@Autowired
+	private CartService cartService;
+	
+	
+	@ResponseBody
+	@PostMapping(value="/.do", produces="application/text; charset=UTF-8")
+	public String getCartList() {
+		cartService.getCartList();
+		
+		return null; 
+	}
+	
+	@RequestMapping("/.do")
+	public String deleteCart(CartVO cartVO) {
+	      cartService.deleteCart(cartVO);
+	      
+	      return null;
+	   }
+	
+	@RequestMapping("/.do")
+	public String insertCart(CartVO cartVO) {
+	      cartService.insertCart(cartVO);
+	      
+	      return null;
+	   }
+	
 }
--- Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/service/cart/CartService.java
+++ Project_SAGAJI/ShoppingMall/src/main/java/com/sagaji/shoppingmall/service/cart/CartService.java
@@ -1,5 +1,10 @@
 package com.sagaji.shoppingmall.service.cart;
 
+import com.sagaji.shoppingmall.vo.CartVO;
+
 public interface CartService {
+	List<CartVO> getCartList(String );
+	void deleteCart(String, int);
+	void insertCart(CartVO cartVO);
 
 }
